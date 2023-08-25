1c1263da24725192efd509882ba47ca39cfb9310
insomnyawolf <insomnyawolf@gmail.com>
2023-08-25 02:58:13 +0200

    Update Autoinstall Scripts

--- InstallGameHelper/ARMV8 autoinstall.bat
+++ InstallGameHelper/ARMV8 autoinstall.bat
@@ -1,4 +1,4 @@
-set "obbname=main.2000084.com.YoStarEN.Arknights.obb"
+set "obbname=main.2000088.com.YoStarEN.Arknights.obb"
 adb devices
 adb install android-prod-arm64.apk
 adb push "%obbname%" "/storage/emulated/0/Android/obb/com.YoStarEN.Arknights/%obbname%"
